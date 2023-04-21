12d5a513efb46d681f02e48924c2852e49aa77b0
Kennedy Musau <kennedy.musauu@gmail.com>
2023-04-21 07:05:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
 // Your code here
+
+function createEmployeeRecord(employeeRecord) {
+  const [firstName, familyName, title, payPerHour] = employeeRecord;
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+function createEmployeeRecords(employeeRecords) {
+  return employeeRecords.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(employeeRecord, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date,
+  });
+  return employeeRecord;
+}
+
+function createTimeOutEvent(employeeRecord, dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date,
+  });
+
+  return employeeRecord;
+}
+
+function hoursWorkedOnDate(employeeRecord, date) {
+  const timeInEvent = employeeRecord.timeInEvents.find(
+    (event) => event.date === date
+  );
+  const timeOutEvent = employeeRecord.timeOutEvents.find(
+    (event) => event.date === date
+  );
+
+  const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+
+  return hoursWorked;
+}
+
+function wagesEarnedOnDate(employeeRecord, date) {
+  const hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+  const payRate = employeeRecord.payPerHour;
+  const wagesEarned = hoursWorked * payRate;
+
+  return wagesEarned;
+}
+
+function allWagesFor(employeeRecord) {
+  const datesWorked = employeeRecord.timeInEvents.map((event) => event.date);
+  const totalWages = datesWorked.reduce((acc, date) => {
+    return acc + wagesEarnedOnDate(employeeRecord, date);
+  }, 0);
+
+  return totalWages;
+}
+
+function calculatePayroll(employeeRecords) {
+  const totalWages = employeeRecords.reduce((acc, record) => {
+    return acc + allWagesFor(record);
+  }, 0);
+
+  return totalWages;
+}
